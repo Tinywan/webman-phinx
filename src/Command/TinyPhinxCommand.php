78885d343ede095449420c5f9474d74a6ed49f41
Tinywan <756684177@qq.com>
2022-03-26 13:34:19 +0800

    tiny:phinx

--- src/Command/TinyPhinxCommand.php
+++ src/Command/TinyPhinxCommand.php
@@ -1,22 +1,30 @@
 <?php
 /**
- * @desc PhinxCommand
+ * @desc TinyPhinxCommand
  * @author Tinywan(ShaoBo Wan)
  * @email 756684177@qq.com
  * @date 2022/3/26 12:58
  */
 declare(strict_types=1);
 
-namespace Tinywan\Phinx;
+namespace Tinywan\Phinx\Command;
 
 use Phinx\Console\PhinxApplication;
-use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PhinxCommand extends Application
+class TinyPhinxCommand extends Command
 {
+    protected static $defaultName = 'tiny:phinx';
+    protected static $defaultDescription = 'tiny:phinx';
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @throws \Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $phinx = new PhinxApplication();
@@ -28,7 +36,6 @@ class PhinxCommand extends Application
             '--configuration' => config_path().'/plugin/tinywan/phinx/phinx.php'
         ];
 
-        $input = new ArrayInput($arguments);
         $returnCode = $command->run(new ArrayInput($arguments), $output);
         // ...
     }
